ba606ed33ae277add027c83ecbe50b22ef169551
Preetham.M.S <preethamms619@gmail.com>
2020-04-25 20:12:46 +0530

    salting and hashing password with bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -37,29 +38,46 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register", function(req, res){
-	const newUser = new User({
-		username: req.body.username,
-		password: md5(req.body.password)
-	});
-	newUser.save(function(err){
+
+	bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
 		if (!err) {
-			console.log("register successfully");
-			res.render("secrets");
+			const newUser = new User({
+				username: req.body.username,
+				password: hash
+			});
+			newUser.save(function(err){
+				if (!err) {
+					console.log("register successfully");
+					res.render("secrets");
+				} else {
+					console.log(err);
+				}
+			});
 		} else {
 			console.log(err);
 		}
+
 	});
+
+	
 });
 
 app.post("/login", function(req, res){
 	User.findOne({username: req.body.username}, function(err, foundUser){
 		if (!err) {
-			if (foundUser.password === md5(req.body.password)) {
-				console.log("Login successful");
-				res.render("secrets");
-			} else {
-				console.log("password incorrect");
+			if (foundUser) {
+				bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+					if (result === true) {
+						console.log("Login successful");
+						res.render("secrets");
+					}else {
+						console.log("password incorrect");
+					}
+
+				});
 			}
+				
 		} else {
 			console.log("username not found");
 		}
@@ -68,5 +86,5 @@ app.post("/login", function(req, res){
 
 
 app.listen(3000, function(){
-   console.log("Server running on port 3000");
+	console.log("Server running on port 3000");
 });
